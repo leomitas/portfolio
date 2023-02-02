dc9aee5309a9b75af6d61d2eb4afb4a7dc73a49d
Leonardo Abreu <leonardo.abreu.dev@gmail.com>
2023-02-02 13:34:11 -0300

    atualização do link do cv

--- src/components/PersonalImage/index.tsx
+++ src/components/PersonalImage/index.tsx
@@ -1,36 +1,36 @@
-import React from "react";
-import personalImage from "../../assets/personalImage.png";
-import { Section } from "./styles";
-import { SiLinkedin, SiGithub } from "react-icons/si";
+import React from 'react'
+import personalImage from '../../assets/personalImage.png'
+import { Section } from './styles'
+import { SiLinkedin, SiGithub } from 'react-icons/si'
 
 const PersonalImage = () => {
   return (
-    <Section className="container">
+    <Section className='container'>
       <div>
         <p>Desenvolvedor Front End</p>
-        <p className="text">
+        <p className='text'>
           Olá, eu sou o Leonardo Abreu - Taubaté, SP e tenho 19 anos. Estou a
           procura do meu primeiro emprego para ingressar na área de programação,
           o qual sou apaixonado desde criança.
         </p>
-        <div className="links">
+        <div className='links'>
           <a
-            href="https://drive.google.com/file/d/1Cb28QLltOPNIKWv2mgTwSLVwQ5iUjdqb/view?usp=share_link"
-            className="cv"
+            href='https://drive.google.com/file/d/18rV9GtaHLwMva2PMEv74HuFVpay1GAZn/view?usp=sharing'
+            className='cv'
           >
             Baixar CV
           </a>
-          <a href="https://www.linkedin.com/in/leonardo-abreu2402/">
-            <SiLinkedin size={40} color="white" />
+          <a href='https://www.linkedin.com/in/leonardo-abreu2402/'>
+            <SiLinkedin size={40} color='white' />
           </a>
-          <a href="https://github.com/leomitas">
-            <SiGithub size={40} color="white" />
+          <a href='https://github.com/leomitas'>
+            <SiGithub size={40} color='white' />
           </a>
         </div>
       </div>
       <img src={personalImage} />
     </Section>
-  );
-};
+  )
+}
 
-export default PersonalImage;
+export default PersonalImage
