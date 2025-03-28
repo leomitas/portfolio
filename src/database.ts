e677ef059b0e9b658a7d55405ca822730600c26a
Leonardo Abreu <leonardo.abreu.dev@gmail.com>
2025-03-28 15:31:51 -0300

    feat: atualizar links das imagens dos projetos

--- src/database.ts
+++ src/database.ts
@@ -1,12 +1,12 @@
 export const projects = [
   {
-    link: 'https://oquebelavive.com.br',
-    title: 'oquebelavive.com.br',
+    link: "https://oquebelavive.com.br",
+    title: "oquebelavive.com.br",
     description:
-      'Desenvolvi um site personalizado para venda de produtos exclusivos utilizando Next.js e Tailwind.. O site apresenta uma interface intuitiva e elegante, destacando os itens à venda.. Além disso, incluí uma seção que exibe postagens do Instagram, permitindo que os visitantes conheçam mais sobre o vendedor.. Para facilitar o contato, também adicionei informações de contato, como instagram e telefone(Whatsapp).. Este projeto foi uma oportunidade incrível para aplicar minhas habilidades de desenvolvimento web e criar algo significativo para alguém.',
+      "Desenvolvi um site personalizado para venda de produtos exclusivos utilizando Next.js e Tailwind.. O site apresenta uma interface intuitiva e elegante, destacando os itens à venda.. Além disso, incluí uma seção que exibe postagens do Instagram, permitindo que os visitantes conheçam mais sobre o vendedor.. Para facilitar o contato, também adicionei informações de contato, como instagram e telefone(Whatsapp).. Este projeto foi uma oportunidade incrível para aplicar minhas habilidades de desenvolvimento web e criar algo significativo para alguém.",
     images: [
-      'https://i.postimg.cc/m254w2Bx/print1.png',
-      'https://i.postimg.cc/LX6QwL3x/print2.png',
+      "https://i.postimg.cc/ZRT6Fpk2/print1.png",
+      "https://i.postimg.cc/y6RSgLSx/print2.png",
     ],
   },
-]
+];
