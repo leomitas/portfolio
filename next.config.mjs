51b656375fc4a3443bf910e2263446cede59ccfd
Leonardo Abreu <leonardo.abreu.dev@gmail.com>
2025-04-10 10:13:28 -0300

    fix: ajuste build next

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: 'i.postimg.cc' }],
+    remotePatterns: [{ hostname: "i.postimg.cc" }],
   },
-  distDir: 'dist',
-}
+};
 
-export default nextConfig
+export default nextConfig;
