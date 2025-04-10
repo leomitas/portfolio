ff4822cff5330000ded7d31d92e2035b048980e6
Leonardo Abreu <leonardo.abreu.dev@gmail.com>
2025-04-10 09:43:55 -0300

    fix: atualizar link do curriculo

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import { LuDownload } from 'react-icons/lu'
+import React from "react";
+import { LuDownload } from "react-icons/lu";
 
 export default function page() {
   return (
-    <section className='flex flex-col items-center p-12 px-4 md:px-0 gap-8'>
+    <section className="flex flex-col items-center p-12 px-4 md:px-0 gap-8">
       <a
-        className='flex items-center gap-2 max-w-max bg-ebony-500 p-2 px-8 rounded-md cursor-pointer'
-        href='https://drive.google.com/file/d/1IMrErndPCsXlmNglUd6t0JjXQZd33nXt/view'
-        target='_blank'
+        className="flex items-center gap-2 max-w-max bg-ebony-500 p-2 px-8 rounded-md cursor-pointer"
+        href="https://drive.google.com/file/d/1wQPeGwiDqW08OpG31JJA20VCLRoKPQWo/view"
+        target="_blank"
       >
         <LuDownload /> Download CV
       </a>
       <iframe
-        src='https://drive.google.com/file/d/1IMrErndPCsXlmNglUd6t0JjXQZd33nXt/preview'
-        allow='autoplay'
+        src="https://drive.google.com/file/d/1wQPeGwiDqW08OpG31JJA20VCLRoKPQWo/preview"
+        allow="autoplay"
         height={1120}
-        className='w-380 md:w-840 h-544 md:h-1120'
+        className="w-380 md:w-840 h-544 md:h-1120"
       ></iframe>
       <a
-        className='flex items-center gap-2 max-w-max bg-ebony-500 p-2 px-8 rounded-md cursor-pointer'
-        href='https://drive.google.com/file/d/1IMrErndPCsXlmNglUd6t0JjXQZd33nXt/view'
-        target='_blank'
+        className="flex items-center gap-2 max-w-max bg-ebony-500 p-2 px-8 rounded-md cursor-pointer"
+        href="https://drive.google.com/file/d/1wQPeGwiDqW08OpG31JJA20VCLRoKPQWo/view"
+        target="_blank"
       >
         <LuDownload /> Download CV
       </a>
     </section>
-  )
+  );
 }
